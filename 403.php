bb0f1b5c2caf10fde9416ca5570e05ac1622a8cd
hberker <hosni.berker@gmail.com>
2019-11-27 00:31:34 -0500

    changes page scale

---
+++ 403.php
@@ -0,0 +1,4 @@
+<div class="text center wordy">
+    <h2>403 Error!</h2>
+    <h3>Whatever your trying to do its not allowed</h3>
+</div>
--- 404.php
+++ 404.php
@@ -1,4 +1,5 @@
 <div class="text center wordy">
-    <h2 >This is not the web page you are looking for</h2>
+    <h2>404 Error!</h2>
+    <h3 >This is not the web page you are looking for</h3>
     <img class="imgcenter" style="width 5em" src="IMG/404img.jpg">
 </div>
