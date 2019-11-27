ca72af9fbfe799fb21fedeb1f61223f956090d33
hberker <hosni.berker@gmail.com>
2019-11-27 00:26:15 -0500

    404 error page

--- 404.php
+++ 404.php
@@ -1,2 +1,4 @@
-<img class="imgcenter" src="IMG/404img.png">
-<h1>This is not the web page you are looking for</h1>
+<div class="text center wordy">
+    <h2 >This is not the web page you are looking for</h2>
+    <img class="imgcenter" style="width 5em" src="IMG/404img.jpg">
+</div>
